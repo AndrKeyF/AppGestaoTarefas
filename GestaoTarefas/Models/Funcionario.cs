59e5a6809694373f7131f2fe7c1b21bf47787127
Andre <1011847@sal.ipg.pt>
2019-11-19 14:43:32 +0000

    Modelo funcionario

---
+++ GestaoTarefas/Models/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefas.Models
+{
+    public class Funcionario
+    {
+        public int FuncionarioId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        public string Telemovel { get; set; }
+        [Required]
+        public string CC { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
