a990f72ce11d1fe864e034c7d660e43bfb58a4b0
cris <crist@LAPTOP-9D4R4P9I>
2019-11-25 10:56:58 +0000

    Modelo Cargos

---
+++ GestaoTarefas/Models/Cargo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefas.Models
+{
+    public class Cargo
+    {
+        public int CargoId { get; set; }
+
+        public string Nome  { get; set; }
+
+     
+
+
+
+
+
+
+    }
+}
