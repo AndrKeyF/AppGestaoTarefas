f82ab71bfd74d237e327df53a40e2f4b7066e8a4
cris <crist@LAPTOP-9D4R4P9I>
2020-01-13 11:20:46 +0000

    alteracao da paginacao

---
+++ GestaoTarefas/Models/PaginationVMCargo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefas.Models
+{
+    public class PaginationVMCargo
+    {
+       
+        public IEnumerable<Cargo> Cargos { get; set; }
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int FirstPageShow { get; set; }
+        public int LastPageShow { get; set; }
+    }
+}
