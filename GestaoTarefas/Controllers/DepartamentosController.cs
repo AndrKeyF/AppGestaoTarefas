046b79447e7568d5f68460ae237f6bacab04d539
Tiago Santos <tiagomiguelgs@hotmail.com>
2020-01-14 15:44:27 +0000

    Departamento

---
+++ GestaoTarefas/Controllers/DepartamentosController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GestaoTarefas.Models;
+
+namespace GestaoTarefas.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        private int NUMBER_PAGES_BEFORE_AND_AFTER = 5;
+        private decimal NUMBER_DEPAR_PER_PAGE = 5;
+        private int DEPAR_PER_PAGE = 5;
+
+        private readonly GestaoTarefasDbContext _context;
+
+        public DepartamentosController(GestaoTarefasDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Departamentos
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            decimal numberDepartamentos = _context.Departamento.Count();
+            PaginationViewModelDepartamento vm = new PaginationViewModelDepartamento
+            {
+                Departamentos = _context.Departamento.OrderBy(p => p.Nome).Skip((page - 1) * DEPAR_PER_PAGE).Take(DEPAR_PER_PAGE),
+                CurrentPage = page,
+                FirstPageShow = Math.Max(1, page - NUMBER_PAGES_BEFORE_AND_AFTER),
+                TotalPages = (int)Math.Ceiling(numberDepartamentos / NUMBER_DEPAR_PER_PAGE)
+            };
+            vm.LastPageShow = Math.Min(vm.TotalPages, page + NUMBER_PAGES_BEFORE_AND_AFTER);
+            return View(vm);
+        }
+
+        // GET: Departamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento
+                .FirstOrDefaultAsync(m => m.DepartamentoId == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamentos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DepartamentoId,Nome")] Departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(departamento);
+                await _context.SaveChangesAsync();
+                return View("NoteCriarD", departamento);
+                //return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento.FindAsync(id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DepartamentoId,Nome")] Departamento departamento)
+        {
+            if (id != departamento.DepartamentoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(departamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DepartamentoExists(departamento.DepartamentoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return View("NoteEditarD", departamento);
+                //return RedirectToAction(nameof(Index));
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var departamento = await _context.Departamento
+                .FirstOrDefaultAsync(m => m.DepartamentoId == id);
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        // POST: Departamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var departamento = await _context.Departamento.FindAsync(id);
+            _context.Departamento.Remove(departamento);
+            await _context.SaveChangesAsync();
+            return View("NoteApagarD", departamento);
+            //return RedirectToAction(nameof(Index));
+        }
+
+        private bool DepartamentoExists(int id)
+        {
+            return _context.Departamento.Any(e => e.DepartamentoId == id);
+        }
+    }
+}
---
+++ GestaoTarefas/Models/Departamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestaoTarefas.Models
+{
+    public class Departamento
+    {
+        public int DepartamentoId { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
